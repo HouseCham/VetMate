052f588e4a6a1fe7538bbc6988dd7c6ce35ae8bb
HouseCham <ramsesramirezvallejo@gmail.com>
2023-04-24 14:30:33 -0600

    common file for controllers generated

---
+++ controllers/user.controller.go
@@ -0,0 +1,11 @@
+package controllers
+
+import (
+	"errors"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func InsertNewUser(c *fiber.Ctx) error {
+	return errors.New("Holis")
+}
--- controllers/vet.controller.go
+++ controllers/vet.controller.go
@@ -1,59 +1,14 @@
 package controllers
 
 import (
-	"database/sql"
-	"errors"
 	"strconv"
 	"strings"
 
 	db "github.com/HouseCham/VetMate/database/sql"
-	"github.com/HouseCham/VetMate/interfaces"
 	"github.com/HouseCham/VetMate/util"
 	"github.com/HouseCham/VetMate/validations"
 	"github.com/gofiber/fiber/v2"
 )
-
-var DB *sql.DB
-var Queries *db.Queries
-
-//? ==================== COMMON ====================
-
-// ShareDbConnection is a function that shares the
-// database connection to all controllers
-// so that they can use the same connection
-func ShareDbConnection(db *sql.DB) {
-	DB = db
-	Queries = createNewQuery()
-}
-
-// createNewQuery is a function that creates a new
-// query object for the database connection
-func createNewQuery() *db.Queries {
-	return db.New(DB)
-}
-
-// TrimInputFields is a function that trims all the
-// input fields from the request body
-// it is an interface function that is used by all
-// the controllers
-func trimInputFields(input interfaces.INewInsertParams) {
-	input.Trim()
-}
-
-// CheckVetEmailAlreadyInUse is a function that checks
-// if the email is already in use by checking the database
-func checkVetEmailAlreadyInUse(email string, c *fiber.Ctx) (string, error) {
-	emailExists, err := Queries.CheckVetEmailExists(c.Context(), email)
-	if err != nil {
-		c.Status(fiber.StatusInternalServerError)
-		return "Error checking email", err
-	} else if emailExists > 0 {
-		return "Error", errors.New("email already in use")
-	}
-	return "", err
-}
-
-//? ==================== VET CONTROLLERS ====================
 
 // InsertNewVet is a function that inserts a new vet
 // to the database
