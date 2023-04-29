28b77e72ba45cddd49c968945b4c607e95aa5acd
HouseCham <ramsesramirezvallejo@gmail.com>
2023-04-29 03:17:25 -0600

    vetPlace controller 1st implement

---
+++ controllers/vetplace.controller.go
@@ -0,0 +1,19 @@
+package controllers
+
+import "github.com/gofiber/fiber/v2"
+
+func InsertVetPlace(c *fiber.Ctx) error {
+	panic("not implemented") // TODO: Implement
+}
+
+func GetVetPlace(c *fiber.Ctx) error {
+	panic("not implemented") // TODO: Implement
+}
+
+func UpdateVetPlace(c *fiber.Ctx) error {
+	panic("not implemented") // TODO: Implement
+}
+
+func DeleteVetPlace(c *fiber.Ctx) error {
+	panic("not implemented") // TODO: Implement
+}
