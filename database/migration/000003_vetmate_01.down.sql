2b9b3badc7e7ec28c0b39b398628513080a864c1
HouseCham <ramsesramirezvallejo@gmail.com>
2023-05-12 16:31:04 -0600

    migrate03 version implemented

---
+++ database/migration/000003_vetmate_01.down.sql
@@ -0,0 +1,2 @@
+DROP TABLE IF EXISTS `vacunaciones`;
+DROP TABLE IF EXISTS `vacunas`;
---
+++ database/migration/000003_vetmate_01.up.sql
@@ -0,0 +1,27 @@
+CREATE TABLE `vacunaciones` (
+  `id` INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+  `mascota_id` INT UNSIGNED,
+  `tipo_vacuna_id` INT UNSIGNED,
+  `fecha_aplicacion` date DEFAULT (now()),
+  `laboratorio` varchar(150) NOT NULL,
+  `lote_vacuna` varchar(255) NOT NULL,
+  `peso` decimal(10,3) NOT NULL,
+  `vacuna_fecha_caducidad` date NOT NULL,
+  `prox_fecha_vacunacion` date NOT NULL
+);
+
+CREATE TABLE `vacunas` (
+  `id` INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+  `vet_id` INT UNSIGNED,
+  `tipo_vacuna` varchar(200) NOT NULL
+);
+
+CREATE INDEX `vacunaciones_index_11` ON `vacunaciones` (`mascota_id`);
+
+CREATE INDEX `vacunaciones_index_12` ON `vacunaciones` (`fecha_aplicacion`);
+
+ALTER TABLE `vacunaciones` ADD FOREIGN KEY (`mascota_id`) REFERENCES `mascotas` (`id`);
+
+ALTER TABLE `vacunaciones` ADD FOREIGN KEY (`tipo_vacuna_id`) REFERENCES `vacunas` (`id`);
+
+ALTER TABLE `vacunas` ADD FOREIGN KEY (`vet_id`) REFERENCES `veterinarios` (`id`);
